f277f43a5bdd271dc2964016a072ca8edb92bac5
Jeremy Mullen <jeremymullen@stockx.com>
2019-06-30 00:16:37 -0400

    'snake' pixel can be moved on canvas

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,36 @@
+const resolution = 10;
+
+let snake;
+
 function setup() {
     createCanvas(400, 400);
+    frameRate(15);
+
+    snake = new Snake();
 }
 
 function draw() {
+    scale(resolution);
     background(220);
+
+    snake.update();
+    snake.show();
+}
+
+function keyPressed() {
+
+    switch (keyCode) {
+        case LEFT_ARROW:
+            snake.setDirection(-1, 0);
+            break;
+        case RIGHT_ARROW:
+            snake.setDirection(1, 0);
+            break;
+        case DOWN_ARROW:
+            snake.setDirection(0, 1);
+            break;
+        case UP_ARROW:
+            snake.setDirection(0, -1);
+            break;
+    }
 }
--- src/snake.js
+++ src/snake.js
@@ -1,7 +1,28 @@
 class Snake {
 
     constructor() {
-    
+        this.x = 1;
+        this.y = 1;
+        this.xDir = 0;
+        this.yDir = 0;
     }
+
+    show() {
+        rect(this.x, this.y, 1, 1);
+    }
+
+    update() {
+        this.x += this.xDir;
+        this.y += this.yDir;
+    }
+
+    setDirection(x, y) {
+        this.xDir = x;
+        this.yDir = y;
+    }
+
+
+
+
 
 }
